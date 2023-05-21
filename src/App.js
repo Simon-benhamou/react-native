f4c27ea9fd948848806ddd138ec8b51e268cc077
Simon-benhamou <simon.benhamou@icloud.com>
2023-05-21 23:03:46 +0300

    fix git

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -8,77 +9,74 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import store from './redux/store';
 import HomeScreen from './screens/HomeScreen';
 import ArticleScreen from './screens/ArticleScreen';
-import HeaderTitle from './components/HeaderTitle'
+import HeaderTitle from './components/HeaderTitle';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
-const CategoryScreen = ({ route }) => {
+
+function CategoryDetailScreen({ route }) {
   const { category } = route.params;
   return (
     <View>
       <Text>{category} Screen</Text>
     </View>
   );
-};
-const HomeStack = () => (
-  <Stack.Navigator>
-    <Stack.Screen
-      name="Home"
-      component={HomeScreen}
-      options={{
-        headerTitle: () => <HeaderTitle />,
-      }}
-    />
-    <Stack.Screen
-      name="Article"
-      component={ArticleScreen}
-      options={{
-        headerTitle: () => <HeaderTitle />,
-      }}
-    />
-  </Stack.Navigator>
-);
+}
 
-const App = () => {
+function MainScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerTitle: () => <HeaderTitle />,
+        }}
+      />
+      <Stack.Screen
+        name="Article"
+        component={ArticleScreen}
+        options={{
+          headerTitle: () => <HeaderTitle />,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function Sidebar() {
+  return (
+    <View>
+      {['Local News', 'Technology', 'Politics', 'Advertisement'].map((category) => (
+        <Text key={category} onPress={() => store.dispatch({ type: 'SET_CATEGORY', payload: category })}>{category}</Text>
+      ))}
+    </View>
+  );
+}
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator drawerContent={Sidebar}drawerPosition="right">
+      <Drawer.Screen name="Home" component={MainScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+function App() {
   return (
     <Provider store={store}>
       <SafeAreaProvider>
         <NavigationContainer>
           <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeStack} />
+            <Tab.Screen name="Home" component={DrawerNavigator}   options={{
+                headerTitle: () => <HeaderTitle />,
+              }}/>
           </Tab.Navigator>
-          <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={HomeStack} />
-            <Drawer.Screen
-              name="Local News"
-              component={CategoryScreen}
-              initialParams={{ category: 'Local News' }}
-              onPress={() => store.dispatch({ type: 'SET_CATEGORY', payload: 'Local News' })}
-            />
-            <Drawer.Screen
-              name="Technology"
-              component={CategoryScreen}
-              initialParams={{ category: 'Technology' }}
-              onPress={() => store.dispatch({ type: 'SET_CATEGORY', payload: 'Technology' })}
-            />
-            <Drawer.Screen
-              name="Politics"
-              component={CategoryScreen}
-              initialParams={{ category: 'Politics' }}
-              onPress={() => store.dispatch({ type: 'SET_CATEGORY', payload: 'Politics' })}
-            />
-            <Drawer.Screen
-              name="Advertisement"
-              component={CategoryScreen}
-              initialParams={{ category: 'Advertisement' }}
-              onPress={() => store.dispatch({ type: 'SET_CATEGORY', payload: 'Advertisement' })}
-            />
-          </Drawer.Navigator>
         </NavigationContainer>
       </SafeAreaProvider>
     </Provider>
   );
-};
+}
 
 export default App;
